1a8979544d0b8f6aa45e67e02f5e9fcc64cde886
Kemal Cleva <kemalcleva69@gmail.com>
2023-06-15 11:32:29 +0200

    yy

--- api/cSupply.js
+++ api/cSupply.js
@@ -6,13 +6,11 @@ module.exports = async (req, res) => {
     const lpSupply = await getLPSupply();
     const airdropSupply = await getAirdropSupply();
     const circulatingSupply = totalSupply - lpSupply - airdropSupply;
-    res.status(200).send({
-      result: circulatingSupply,
-    });
+    const circulatingSupplyNumber = Number(circulatingSupply) / 10**18; // Assume 18 decimal places
+    res.send(circulatingSupplyNumber.toFixed(8)); // Send as text
   } catch (err) {
-    res.status(500).send({
-      message: "Error fetching data"
-    });
+    console.error(err);
+    res.status(500).send("Error fetching data");
   }
 };
 
@@ -20,19 +18,19 @@ async function getTotalSupply() {
   const response = await axios.get(
     "https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=0x3a36dc12efaa14a3f692b94f97450594459661b6&apikey=929FQNKF895HQGVBAZBM4J5M9X7T73N8BN"
   );
-  return BigInt(response.data.result);
+  return response.data && response.data.result ? BigInt(response.data.result) : BigInt(0);
 }
 
 async function getLPSupply() {
   const response = await axios.get(
     "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x3a36dc12eFaa14a3F692B94f97450594459661b6&address=0x84E7380DE236cD12B312E078d99d5bb9bA68Bf3b&tag=latest&apikey=929FQNKF895HQGVBAZBM4J5M9X7T73N8BN"
   );
-  return BigInt(response.data.result);
+  return response.data && response.data.result ? BigInt(response.data.result) : BigInt(0);
 }
 
 async function getAirdropSupply() {
   const response = await axios.get(
     "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x3a36dc12eFaa14a3F692B94f97450594459661b6&address=0x407993575c91ce7643a4d4cCACc9A98c36eE1BBE&tag=latest&apikey=929FQNKF895HQGVBAZBM4J5M9X7T73N8BN"
   );
-  return BigInt(response.data.result);
+  return response.data && response.data.result ? BigInt(response.data.result) : BigInt(0);
 }
